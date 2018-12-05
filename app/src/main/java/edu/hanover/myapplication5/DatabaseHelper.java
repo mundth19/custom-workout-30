8ade7434041f5b35b3d9d71ef424e56ca84eb47c
Hannah Mundt <mundth19@hanover.edu>
2018-12-04 21:25:12 -0500

    Initial commit

---
+++ app/src/main/java/edu/hanover/myapplication5/DatabaseHelper.java
@@ -0,0 +1,44 @@
+package edu.hanover.myapplication5;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.content.ContentValues;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "workouts"; // the name of our database
+    private static final int DB_VERSION = 1;
+    DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE CARDIO ("
+                + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + " NAME TEXT, "
+                + "LEVEL TEXT, "
+                + "INTENSITY TEXT, "
+                + "DESCRIPTION TEXT);");
+
+        ContentValues cardioValues = new ContentValues();
+        cardioValues.put("NAME", "Running");
+        cardioValues.put("LEVEL", "Beginner");
+        cardioValues.put("INTENSITY", "12min/mile");
+        cardioValues.put("DESCRIPTION", "Run at a 12 minute mile pace for 10 minutes.");
+        db.insert("CARDIO", null, cardioValues);
+
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {    } }
+
+
+//public class DataActivity extends Activity {
+
+//@Override
+//protected void onCreate(Bundle savedInstanceState) {
+//super.onCreate(savedInstanceState);
+//setContentView(R.layout.activity_data);
+//}
+//}
