e646fea31d3a97199caeb455f03bed4444c88ca4
Hannah Mundt <mundth19@hanover.edu>
2019-01-02 22:54:54 -0500

    created all exercise database tables

--- app/src/main/java/edu/hanover/myapplication5/DatabaseHelper.java
+++ app/src/main/java/edu/hanover/myapplication5/DatabaseHelper.java
@@ -26,9 +26,24 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         insertCardio(db, "Biking", "Beginner", "Bike at a 10 mph speed for 10 minutes", 4);
         insertCardio(db, "Biking", "Intermediate", "Bike at a 13 mph speed for 10 minutes", 5);
         insertCardio(db, "Biking", "Advanced", "Bike at a 15 mph speed for 10 minutes", 6);
-        insertCardio(db, "Elliptical", "Beginner", "Exercise on an elliptical at speed 5 for 10 minutes", 7);
+        insertCardio(db, "Elliptical","Beginner", "Exercise on an elliptical at speed 5 for 10 minutes", 7);
         insertCardio(db, "Elliptical", "Intermediate", "Exercise on an elliptical at speed 7 for 10 minutes", 8);
         insertCardio(db, "Elliptical", "Advanced", "Exercise on an elliptical at speed 9 for 10 minutes", 9);
+
+        db.execSQL("CREATE TABLE LIFTING ("
+                + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + " NAME TEXT, "
+                + "INTENSITY TEXT, "
+                + "DESCRIPTION TEXT);");
+        insertLifting(db, "Upper Body", "Beginner", "8 bicep curls per arm; 3 sets", 1);
+        //insert rest of lifting exercises here
+
+        db.execSQL("CREATE TABLE YOGA ("
+                + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + " NAME TEXT, "
+                + "INTENSITY TEXT, "
+                + "DESCRIPTION TEXT);");
+        //insert yoga exercises here
 
     }
     @Override
@@ -36,18 +51,30 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static void insertCardio (SQLiteDatabase db, String name, String intensity, String description, int resourceId ){
         ContentValues cardioValues = new ContentValues();
+        cardioValues.put("_id", resourceId);
         cardioValues.put("NAME", name);
         cardioValues.put("INTENSITY", intensity);
         cardioValues.put("DESCRIPTION", description);
         db.insert("CARDIO", null, cardioValues);
-    }}
+    }
+    private static void insertLifting (SQLiteDatabase db, String name, String intensity, String description, int resourceId ){
+        ContentValues liftingValues = new ContentValues();
+        liftingValues.put("_id", resourceId);
+        liftingValues.put("NAME", name);
+        liftingValues.put("INTENSITY", intensity);
+        liftingValues.put("DESCRIPTION", description);
+        db.insert("LIFTING", null, liftingValues);
+    }
+
+    private static void insertYoga (SQLiteDatabase db, String name, String intensity, String description, int resourceId ) {
+        ContentValues yogaValues = new ContentValues();
+        yogaValues.put("_id", resourceId);
+        yogaValues.put("NAME", name);
+        yogaValues.put("INTENSITY", intensity);
+        yogaValues.put("DESCRIPTION", description);
+        db.insert("YOGA", null, yogaValues);
+    }
 
 
-//public class DataActivity extends Activity {
+}
 
-//@Override
-//protected void onCreate(Bundle savedInstanceState) {
-//super.onCreate(savedInstanceState);
-//setContentView(R.layout.activity_data);
-//}
-//}
