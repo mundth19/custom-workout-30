5b69cdb7d3fc1cf8accc19b2fcb1069bd6c549a9
Hannah Mundt <mundth19@hanover.edu>
2019-01-29 10:21:35 -0500

    fixed buttons on muscle group page to listview to pull from database

--- app/src/main/java/edu/hanover/myapplication5/MuscleGroup.java
+++ app/src/main/java/edu/hanover/myapplication5/MuscleGroup.java
@@ -2,12 +2,53 @@ package edu.hanover.myapplication5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.view.View;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.CursorAdapter;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+import android.widget.SimpleCursorAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MuscleGroup extends AppCompatActivity {
+
+    ListView lv;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_muscle_group);
+        setContentView(R.layout.activity_show_muscle_group_items);
+
+        lv = (ListView)findViewById(R.id.listview3);
+        db = openOrCreateDatabase("workouts",MODE_PRIVATE,null);
+        Cursor c = db.rawQuery("select name from lifting", null);
+        ArrayList ar = new ArrayList();
+        while (c.moveToNext()) {
+            if (!(ar.contains(c.getString(0)))){
+                ar.add(c.getString(0));
+            }
+        }
+        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_list_item_1,ar);
+        lv.setAdapter(ad);
+
     }
-}
+    }
