000bc2b7ab076856d600b6807e833fdba26a2a71
Hannah Mundt <mundth19@hanover.edu>
2019-03-31 23:08:20 -0400

    Ref #12
    finished history test file and exercise select test file

--- app/src/test/java/edu/hanover/myapplication5/ExerciseSelectTest.java
+++ app/src/test/java/edu/hanover/myapplication5/ExerciseSelectTest.java
@@ -1,6 +1,12 @@
 package edu.hanover.myapplication5;
 
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -8,5 +14,14 @@ public class ExerciseSelectTest {
 
     @Test
     public void onCreate() {
+        //testing the add method used in ExerciseSelect class
+        ArrayList<String> adapter = new ArrayList<>();
+        adapter.add("Running");
+        adapter.add("Biking");
+        adapter.add("Elliptical");
+        assert(adapter.get(0).equals("Running"));
+        assert(adapter.get(1).equals("Biking"));
+        assert(adapter.get(2).equals("Elliptical"));
+
     }
 }
--- app/src/test/java/edu/hanover/myapplication5/HistoryTest.java
+++ app/src/test/java/edu/hanover/myapplication5/HistoryTest.java
@@ -1,20 +1,36 @@
 package edu.hanover.myapplication5;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 import org.junit.Test;
 
+import static android.content.Context.MODE_PRIVATE;
+import static edu.hanover.myapplication5.DatabaseHelper.insertHistory;
 import static org.junit.Assert.*;
 
 public class HistoryTest {
 
     @Test
     public void onCreate() {
-    }
+        SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase("saved",
+                null, null);
+        insertHistory(db, "date", "running", "lifting", "yoga",
+                "notes");
+        Cursor h = db.query("SAVED", new String[] {"DATE", "CARDIO", "LIFT", "YOGA"},
+                null, null, null, null, null);
+        assert (h.moveToFirst() == true); //the first entry will be successfully stored in the database
 
-    @Test
-    public void onOptionsItemSelected() {
+
     }
 
     @Test
     public void onClickClearHistory() {
+        SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase("saved",
+                null, null);
+        db.delete("saved", null, null);
+        Cursor h = db.query("SAVED", new String[] {"DATE", "CARDIO", "LIFT", "YOGA"},
+                null, null, null, null, null);
+        assert(h.moveToFirst() == false); //this means the database will be empty
     }
 }
