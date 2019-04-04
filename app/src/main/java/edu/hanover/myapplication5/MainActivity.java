e36b84dde6ab711a2dc9c7aabc669fb4f4595b48
Hannah Mundt <mundth19@hanover.edu>
2019-04-04 18:51:37 -0400

    Ref #29
    got pop-up window to show with click of 'info' button

--- app/src/main/java/edu/hanover/myapplication5/MainActivity.java
+++ app/src/main/java/edu/hanover/myapplication5/MainActivity.java
@@ -5,7 +5,12 @@ package edu.hanover.myapplication5;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,16 +19,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     //on click, takes user to create a workout
-    public void onClickNewWorkout(View v){
+    public void onClickNewWorkout(View v) {
         Intent intent = new Intent(this, ExerciseSelect.class);
         startActivity(intent);
     }
+
     //on click, takes user to view history
-    public void onClickHistory(View v){
+    public void onClickHistory(View v) {
         Intent intent = new Intent(this, History.class);
         startActivity(intent);
     }
 
+    public void onButtonShowPopupWindowClick(View view) {
 
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.info, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+    }
 }
